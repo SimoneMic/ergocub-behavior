72bf09e765506239fa3fe59f001ceb94532ff1c0
SimoneMic <simone_micheletti@outlook.it>
2024-04-04 10:06:02 +0000

    first tmp commit for ros selectable communication

--- bts/src/conditions/robot_navigating.cpp
+++ bts/src/conditions/robot_navigating.cpp
@@ -6,33 +6,69 @@
 #include <thread>
 #include <yarp/os/Network.h>
 #include <yarp/os/Port.h>
+#include <yarp/os/LogStream.h>
 
 #include <iostream>
-
 
 RobotNavigating::RobotNavigating(string name, const NodeConfiguration& nc, pt::ptree bt_config) :
     ConditionNode(name, nc),
     bt_config(bt_config)
 {
+#if USE_ROS
+#else
     writer_name =  bt_config.get<std::string>("components.navigation.port");
     reader_name = "/BT/" + name;
 
     reader_port.open(reader_name);
+#endif
 }
 
 
 NodeStatus RobotNavigating::tick()
 {
+#if USE_ROS
+
+#else
     yarp::os::Bottle *data = reader_port.read(false);
-    
     if (data == nullptr){
         return NodeStatus::SUCCESS;
     }
 
-    if (data->get(0).asInt16() == 0){
+    switch (data->get(0).asInt32())
+    {
+    case 0: // STATUS_UNKNOWN
         return NodeStatus::SUCCESS;
+        break;
+    case 1: // STATUS_ACCEPTED The goal has been accepted and is awaiting execution
+        return NodeStatus::FAILURE;
+        break;
+    case 2: // STATUS_EXECUTING The goal is currently being executed by the action server
+        return NodeStatus::FAILURE;
+        break;
+    case 3: // STATUS_CANCELING The client has requested that the goal be canceled 
+        return NodeStatus::SUCCESS;
+        break;
+    case 4: // STATUS_SUCCEEDED The goal was achieved successfully by the action server
+        return NodeStatus::SUCCESS;
+        break;
+    case 5: // STATUS_CANCELED The goal was canceled after an external request from an action client
+        return NodeStatus::SUCCESS;
+        break;
+    case 6: // STATUS_ABORTED The goal was terminated by the action server without an external request
+        return NodeStatus::SUCCESS;
+        break;
+    default:
+        yWarning() << "[RobotNavigating::tick] Received an unexpected value from the navigation status: " << data->get(0).asInt32() << " Carrying on...";
+        return NodeStatus::SUCCESS;
+        break;
     }
 
-    return NodeStatus::FAILURE;
+
+    //if (data->get(0).asInt16() == 0){
+    //    return NodeStatus::SUCCESS;
+    //}
+
+    //return NodeStatus::FAILURE;
+#endif
 }
 
--- bts/src/conditions/robot_navigating.h
+++ bts/src/conditions/robot_navigating.h
@@ -13,6 +13,66 @@ using namespace std;
 #include <boost/property_tree/ptree.hpp>
 namespace pt = boost::property_tree;
 
+#if USE_ROS
+#include "rclcpp/rclcpp.hpp"
+//#include "rclcpp_action/rclcpp_action.hpp"
+#include "action_msgs/msg/goal_status_array.hpp"
+#include <memory>
+
+class ros_node : public rclcpp::Node{
+private:
+    rclcpp::Subscription<action_msgs::msg::GoalStatusArray>::SharedPtr m_navigation_result_sub;
+    int m_status;
+public:
+    ros_node() : rclcpp::Node("bt_ros_node_navigation"){
+    m_status = 0;
+    m_navigation_result_sub = this->create_subscription<action_msgs::msg::GoalStatusArray>(
+                "navigate_to_pose/_action/status",
+                rclcpp::SystemDefaultsQoS(),
+                [this](const action_msgs::msg::GoalStatusArray::SharedPtr msg) {   
+                    RCLCPP_INFO( this->get_logger(), "GOAL STATUS: %i", msg->status_list.back().status);
+                    m_status = msg->status_list.back().status;
+                });
+};
+
+
+int get_status(){
+    return m_status;
+    /*switch (m_status)
+    {
+    case 0: // STATUS_UNKNOWN
+        return NodeStatus::SUCCESS;
+        break;
+    case 1: // STATUS_ACCEPTED The goal has been accepted and is awaiting execution
+        return NodeStatus::FAILURE;
+        break;
+    case 2: // STATUS_EXECUTING The goal is currently being executed by the action server
+        return NodeStatus::FAILURE;
+        break;
+    case 3: // STATUS_CANCELING The client has requested that the goal be canceled 
+        return NodeStatus::SUCCESS;
+        break;
+    case 4: // STATUS_SUCCEEDED The goal was achieved successfully by the action server
+        return NodeStatus::SUCCESS;
+        break;
+    case 5: // STATUS_CANCELED The goal was canceled after an external request from an action client
+        return NodeStatus::SUCCESS;
+        break;
+    case 6: // STATUS_ABORTED The goal was terminated by the action server without an external request
+        return NodeStatus::SUCCESS;
+        break;
+    default:
+        yWarning() << "[ros_node::get_status] Received an unexpected value from the navigation status: " << m_status << " Carrying on...";
+        return NodeStatus::SUCCESS
+        break;
+    }*/
+};
+};
+
+
+
+#endif
+
 class RobotNavigating :  public ConditionNode
 {
 public:
@@ -20,9 +80,14 @@ public:
     NodeStatus tick() override;
 private:
     pt::ptree bt_config;
+#if USE_ROS
+    ros_node m_node;
+#else
     yarp::os::Network yarp;
 
     std::string reader_name;
     std::string writer_name;
     yarp::os::BufferedPort<yarp::os::Bottle> reader_port;
+#endif
 };
+
